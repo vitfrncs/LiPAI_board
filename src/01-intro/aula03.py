3983054e3c73b751efca647e5f1cf0abcc401335
vitfrncs <vitoria.fernandes@ufu.br>
2025-07-06 19:12:54 -0300

    aula 03 - comentarios

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,16 @@
+"""Aula 03 - Comentários"""
+
+#Comentário de única linha
+
+'''
+Isso é um comentário de 
+multiplas linhas
+'''
+
+#CRTL / --> comenta partes do código que foram selecionadas
+
+
+n1 = 10
+n2 = 30
+
+# Boas práticas:
