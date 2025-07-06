30fb0e0e77db421ddefc03f569a0a7779b4e2f1f
vitfrncs <vitoria.fernandes@ufu.br>
2025-07-06 19:12:09 -0300

    aula 02 - keywords e identificadores

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,20 @@
+"""Aula 02 - Keywords e identificadores"""
+
+# Palavras reservadas
+#
+
+# Identificadores
+# nome de variáveis, funções, classes
+# case sensitive
+# devem iniciar com uma letra ou _
+# não pode ter espaço em branco
+# não pode usar caracteres especiais
+
+# Boas práticas (clean code):
+c = 10
+contador = 10
+
+# Constantes:
+# letras maiuscúlas
+PI = 3.14
+
