0f4663b13bcca858d1a2b47d68b43e3533fecc52
vitfrncs <vitoria.fernandes@ufu.br>
2025-07-08 22:44:57 -0300

    aula 04 - while

---
+++ src/02-controle-de-fluxo/aula04.py
@@ -0,0 +1,18 @@
+""" Aula 04 - while"""
+
+# while condicao:
+#   instrucao
+#   instrucao
+#   instrucao
+
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1
+
+print('fim :D')
+
+# linha = arquivo.readline()
+# while linha: # string vazia é false
+#   instrucao linha
+#   linha = arquivo.readline()
